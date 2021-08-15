42bde67406c32c1b7ef1ab445c421dc10461502d
gualterapple <39972000+gualterapple@users.noreply.github.com>
2021-08-15 19:00:50 +0100

    LoginPage feita

--- DinisProjecto4/DinisProjecto4/App.xaml.cs
+++ DinisProjecto4/DinisProjecto4/App.xaml.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using DinisProjecto4.Views;
 using Xamarin.Forms;
 using Xamarin.Forms.Xaml;
 
@@ -10,7 +11,7 @@ namespace DinisProjecto4
         {
             InitializeComponent();
 
-            MainPage = new MainPage();
+            MainPage = new LoginPage();
         }
 
         protected override void OnStart()
---
+++ DinisProjecto4/DinisProjecto4/ViewModels/MainViewModel.cs
@@ -0,0 +1,10 @@
+﻿using System;
+namespace DinisProjecto4.ViewModels
+{
+    public class MainViewModel
+    {
+        public MainViewModel()
+        {
+        }
+    }
+}
